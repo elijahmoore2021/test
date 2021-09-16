e5db15a1e7e3e2c7044df3a2ee1433f80ee44329
elijahmoore2021 <83997943+elijahmoore2021@users.noreply.github.com>
2021-09-16 16:44:38 +0530

    Update test.go

--- test.go
+++ test.go
@@ -1,9 +1,34 @@
+/*
+Package stats is a well tested and comprehensive
+statistics library package with no dependencies.
+Example Usage:
+	// start with some source data to use
+	data := []float64{1.0, 2.1, 3.2, 4.823, 4.1, 5.8}
+	// you could also use different types like this
+	// data := stats.LoadRawData([]int{1, 2, 3, 4, 5})
+	// data := stats.LoadRawData([]interface{}{1.1, "2", 3})
+	// etc...
+	median, _ := stats.Median(data)
+	fmt.Println(median) // 3.65
+	roundedMedian, _ := stats.Round(median, 0)
+	fmt.Println(roundedMedian) // 4
+MIT License Copyright (c) 2014-2020 Montana Flynn (https://montanaflynn.com)
+*/
 package test
 
 import "fmt"
 
 
-// It is a test resp
-func print(s string) {
+// It is a test repo
+// hemlo
+// how are you
+func print(s string) string {
   fmt.Println(s)
+  return s
+}
+
+// it's second function
+// efefhef
+func eval(s string) string {
+  print(s)
 }
